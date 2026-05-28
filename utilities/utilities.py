32a1f4c54943cb617fcc64e99d988ed1a5022dc7
itsreallyhex <a.a.alaniz27@email.com>
2026-05-28 07:37:04 +0300

    Add checkcode object

---
+++ utilities/utilities.py
@@ -0,0 +1,5 @@
+# I use this if i am unsure if the command was loaded or not, it will print in the console if the command was loaded
+class checkcode:
+    def __init__(self):
+        print("Command loaded successfully")
+        return
