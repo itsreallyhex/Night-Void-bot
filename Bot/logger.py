c342cd2b4673848e10d1dab36ad55c0daf4f9061
Elhexo <a.a.alaniz27@gmail.com>
2026-05-30 14:41:24 +0300

    Fix logger writing to stderr causing Railway to misclassify INFO logs as errors for some reasons

--- Bot/logger.py
+++ Bot/logger.py
@@ -1,8 +1,10 @@
 import logging
+import sys
 
 def setup_logger(name: str) -> logging.Logger:
     logging.basicConfig(
         level=logging.INFO,
-        format="%(asctime)s [%(levelname)s] %(name)s: %(message)s"
+        format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+        stream=sys.stdout
     )
     return logging.getLogger(name)
